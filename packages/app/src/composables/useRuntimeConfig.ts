f346e726c870918d0d58651937fbbc9d129bc4a3
Nurdism <craigbett@gmail.com>
2026-02-06 13:52:24 -0500

    update default configs

--- packages/app/src/composables/useRuntimeConfig.ts
+++ packages/app/src/composables/useRuntimeConfig.ts
@@ -1,21 +1,48 @@
 import type { NetworkConfig, RuntimeConfig } from "@/configs";
 
 import { checksumAddress } from "@/utils/formatters";
+
 export const DEFAULT_NETWORK: NetworkConfig = {
-  groupId: "era",
-  apiUrl: "https://block-explorer-api.sepolia.zksync.dev",
-  verificationApiUrl: "https://explorer.sepolia.era.zksync.dev",
-  bridgeUrl: "https://portal.zksync.io/bridge/?network=sepolia",
-  hostnames: ["https://sepolia.explorer.zksync.io"],
+  groupId: "battlechain",
+  apiUrl: "https://block-explorer-api.testnet.battlechain.com",
+  verificationApiUrl: "https://block-explorer-api.testnet.battlechain.com/api",
+  bridgeUrl: "https://portal.battlechain.com/bridge/?network=sepolia",
+  hostnames: ["https://block-explorer.testnet.battlechain.com"],
   icon: "/images/icons/zksync-arrows.svg",
   l1ExplorerUrl: "https://sepolia.etherscan.io",
   l2ChainId: 300,
-  l2NetworkName: "ZKsync Era Sepolia Testnet",
+  l2NetworkName: "Battle Chain Testnet",
   maintenance: false,
-  name: "sepolia",
+  name: "testnet",
   published: true,
-  rpcUrl: "https://sepolia.era.zksync.dev",
+  rpcUrl: "http://testnet.battlechain.com",
   baseTokenAddress: checksumAddress("0x000000000000000000000000000000000000800A"),
+  excludedFromBattlechain: [
+    checksumAddress("0x0000000000000000000000000000000000008001"),
+    checksumAddress("0x0000000000000000000000000000000000008002"),
+    checksumAddress("0x0000000000000000000000000000000000008003"),
+    checksumAddress("0x0000000000000000000000000000000000008004"),
+    checksumAddress("0x0000000000000000000000000000000000008005"),
+    checksumAddress("0x0000000000000000000000000000000000008006"),
+    checksumAddress("0x0000000000000000000000000000000000008007"),
+    checksumAddress("0x0000000000000000000000000000000000008008"),
+    checksumAddress("0x0000000000000000000000000000000000008009"),
+    checksumAddress("0x000000000000000000000000000000000000800A"),
+    checksumAddress("0x000000000000000000000000000000000000800B"),
+    checksumAddress("0x000000000000000000000000000000000000800C"),
+    checksumAddress("0x000000000000000000000000000000000000800D"),
+    checksumAddress("0x000000000000000000000000000000000000800E"),
+    checksumAddress("0x000000000000000000000000000000000000800F"),
+    checksumAddress("0x0000000000000000000000000000000000008010"),
+    checksumAddress("0x0000000000000000000000000000000000008011"),
+    checksumAddress("0x0000000000000000000000000000000000008012"),
+    checksumAddress("0x0000000000000000000000000000000000010000"),
+    checksumAddress("0x0000000000000000000000000000000000010001"),
+    checksumAddress("0x0000000000000000000000000000000000010002"),
+    checksumAddress("0x0000000000000000000000000000000000010003"),
+    checksumAddress("0x0000000000000000000000000000000000010004"),
+    checksumAddress("0x0000000000000000000000000000000000010005")
+  ],
 };
 
 export default (): RuntimeConfig => {
